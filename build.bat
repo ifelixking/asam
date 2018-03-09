4fd8283e258602700bd1e9f0eb720bd6c87d7a04
felix <felixking_powerbase@hotmail.com>
2018-03-09 21:28:44 +0800

    md build.bat

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ copy .\src\*.js .\dist
 
 SET ExpFuncs="['_main','_as_test']"
 
-emcc -o ./dist/as.js -s WASM=1 -s EXPORTED_FUNCTIONS=%ExpFuncs% -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" ./src/interface.c
+emcc -o ./dist/as.js -s WASM=1 -s USE_WEBGL2=1 -s EXPORTED_FUNCTIONS=%ExpFuncs% -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" ./src/interface.c
